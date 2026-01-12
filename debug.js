a615c4baeb54d1f0f78978f9af334446624479bd
nekkochan0x0007 <thanhdz167@gmail.com>
2026-01-12 21:36:55 +0700

    modified:   debug.js

--- debug.js
+++ debug.js
@@ -4,18 +4,17 @@ const config = {
     imap: {
         user: 'admin@javalorant.xyz',
         password: 'Nekkochan0x0007@!',
-        host: '172.20.0.2',
-        port: 993,
-        tls: true,
-        tlsOptions: { rejectUnauthorized: false },
+        host: '172.20.0.2', // Giữ nguyên IP nội bộ này
+        port: 143,          // <--- ĐỔI SANG 143
+        tls: false,         // <--- TẮT SSL
         authTimeout: 10000
     }
 };
 
-console.log("Dang ket noi thu den IMAP 993...");
+console.log("Dang ket noi thu den IMAP 143 (No SSL)...");
 
 imaps.connect(config).then(connection => {
-    console.log(">>> KẾT NỐI THÀNH CÔNG! (Pass đúng, Server ngon)");
+    console.log(">>> KẾT NỐI THÀNH CÔNG! (Đường 143 thông thoáng)");
     return connection.end();
 }).catch(err => {
     console.log(">>> KẾT NỐI THẤT BẠI!");
